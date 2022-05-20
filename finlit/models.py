95509dca1ecf2de6cc03f6c7cdb99d58aaa81518
shelen ells <shelen.ells@gmail.com>
2022-05-19 22:52:53 -0700

    adjustments

--- finlit/models.py
+++ finlit/models.py
@@ -9,15 +9,60 @@ from django.db import models
 
 class Profile(models.Model):
     name = models.CharField(max_length=100)
-    income = models.PositiveIntegerField(null=True)
-    expenses = models.SmallIntegerField(null=True)
+    expenses = models.ForeignKey()
 
-    def get_monthly_salary(self):
-        return self.salary / 12
+
+    @property
+    def total_expenses(self):
+        return sum(self.expenses)
+        
     
+    def __str__(self):
+        return str(self.name)    
+
+# make a model for income and for expenses foreign key relationship.
+
+
+class Income(models.Model):
+    type_income =[('ACTIVE','Active'), ('PASSIVE', 'Passive')]
+    profile = models.ForeignKey("Profile", related_name="income", on_delete=models.CASCADE)
+    income_type = models.CharField(max_length=10, choices=type_income, default="ACTIVE") 
+        # i want this to be a drop down that selects either, active income
+        # passive income, dividends, rental income 
+    
+    amount = models.PositiveIntegerField(null=True)
+    # need this to be a foreign key- and i need it 
+    date = models.DateField(null=True)
+    # def get_monthly_salary(self):
+    #     return self.salary / 12
+
+    @property
+    def monthly_income(self):
+        total = 0
+        for item in Income:
+            total += item.amount
+
+
+    def total_income(self):
+        return sum(self.income) 
+
+
     def __str__(self):
         return str(self.name)
 
 
-    # def get_monthly_expenses(self):
-    #     return self.expenses 
+class Expenses(models.Model):
+    type_expense =[('BASE','Base Expenses'), ('Luxury', 'Luxury Expenses')]
+
+    expenses_type = models.CharField(max_length=10, choices=type_expense, default="BASE")
+        # i want this to be a drop down that selects either, active income
+        # passive income, dividends, rental income 
+    
+    debt = models.PositiveIntegerField(null=True)
+    # need this to be a foreign key- and i need it 
+    date = models.DateField(null=True)
+    # def get_monthly_salary(self):
+def total_expenses(self):
+        return sum(self.expenses)
+        
+
