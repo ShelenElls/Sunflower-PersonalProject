99bde411cb3d82256a990eba358635d23e4aa3c3
shelen ells <shelen.ells@gmail.com>
2022-05-20 14:30:42 -0700

    updates

--- finlit/models.py
+++ finlit/models.py
@@ -9,8 +9,7 @@ from django.db import models
 
 class Profile(models.Model):
     name = models.CharField(max_length=100)
-    expenses = models.ForeignKey()
-
+    desciption = models.TextField()
 
     @property
     def total_expenses(self):
@@ -25,16 +24,11 @@ class Profile(models.Model):
 
 class Income(models.Model):
     type_income =[('ACTIVE','Active'), ('PASSIVE', 'Passive')]
-    profile = models.ForeignKey("Profile", related_name="income", on_delete=models.CASCADE)
-    income_type = models.CharField(max_length=10, choices=type_income, default="ACTIVE") 
-        # i want this to be a drop down that selects either, active income
-        # passive income, dividends, rental income 
-    
+    profile = models.ManyToManyField("Profile", related_name="income")
+    income_type = models.CharField(max_length=10, choices=type_income, default="ACTIVE")     
     amount = models.PositiveIntegerField(null=True)
-    # need this to be a foreign key- and i need it 
     date = models.DateField(null=True)
-    # def get_monthly_salary(self):
-    #     return self.salary / 12
+
 
     @property
     def monthly_income(self):
@@ -48,21 +42,23 @@ class Income(models.Model):
 
 
     def __str__(self):
-        return str(self.name)
+        return str(self.income_type)
 
 
 class Expenses(models.Model):
     type_expense =[('BASE','Base Expenses'), ('Luxury', 'Luxury Expenses')]
-
     expenses_type = models.CharField(max_length=10, choices=type_expense, default="BASE")
-        # i want this to be a drop down that selects either, active income
-        # passive income, dividends, rental income 
-    
+    profile = models.ManyToManyField("Profile", related_name="expenses")
     debt = models.PositiveIntegerField(null=True)
     # need this to be a foreign key- and i need it 
     date = models.DateField(null=True)
     # def get_monthly_salary(self):
-def total_expenses(self):
+    @property
+    def total_expenses(self):
         return sum(self.expenses)
-        
+
+    def __str__(self):
+        return str(self.expenses_type)
+
+            
 
